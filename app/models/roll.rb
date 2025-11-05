0b4ac1aaad48f01f968137468cfe71d4f7af6ca3
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-05 14:39:07 -0500

    main flew too close to the sun; keep it simple stupid

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -2,16 +2,10 @@ class Roll < ApplicationRecord
   belongs_to :randomizer
   has_many :results
   validates :name, presence: true
-  attribute :dice, :json, default: []
-
-  def combinations
-    # placeholder
-    4
-  end
 
   def results_with_placeholders
     temp = results
-    while temp.size < combinations
+    while temp.size < (dice || 2)
       temp << results.build
     end
     return temp
---
+++ db/migrate/20251105193726_change_rolls_dice_to_integer.rb
@@ -0,0 +1,6 @@
+class ChangeRollsDiceToInteger < ActiveRecord::Migration[8.1]
+  def change
+    remove_column :rolls, :dice
+    add_column :rolls, :dice, :integer
+  end
+end
