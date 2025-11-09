f05017afea2ef7785ab74c2c104c5e91acadca40
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-09 13:21:50 -0500

    20251109-two test fix

--- test/models/result_test.rb
+++ test/models/result_test.rb
@@ -2,14 +2,15 @@ require "test_helper"
 
 class ResultTest < ActiveSupport::TestCase
   test "result value must fall within roll dice range" do
-    roll = rolls(:encounter_distance) # d6
+    roll = rolls(:encounter_distance) # 2d6
 
-    result = roll.results.build(name: "out of range", value: 7)
+    result = roll.results.build(name: "out of range", value: 13)
 
-    assert_not result.valid?, "Result with value out of range should be invalid"
+    assert_not result.valid?,
+      "Result with value out of range should be invalid"
 
     assert_includes result.errors[:value],
-      "must be less than or equal to #{roll.dice}",
+      "must be less than or equal to #{roll.dice_object.max}",
       "Validation error should mention dice limit"
   end
 end
