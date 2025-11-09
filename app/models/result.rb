7df578461fafa8d1dc31fdcaf5fd94fc07ced1ed
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-09 08:31:33 -0500

    main first test passing

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,20 @@
 class Result < ApplicationRecord
   belongs_to :roll
+
+  validates :name, presence: true
+  validates :value, presence: true
+  validate :value_within_dice_range
+
+  private
+
+  def value_within_dice_range
+    return if value.blank? || roll.blank?
+
+    if value <= 0
+      errors.add(:value, "must be greater than 0")
+    elsif roll.dice && value > roll.dice
+      errors.add(:value, "must be less than or equal to #{roll.dice}")
+    end
+  end
 end
+
--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,4 +1,7 @@
 class Roll < ApplicationRecord
   belongs_to :randomizer
   has_many :results
+
+  validates :name, presence: true
+  validates :dice, presence: true
 end
