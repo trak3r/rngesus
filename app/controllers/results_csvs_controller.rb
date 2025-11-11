700dfd1e6c3f15b16631d7b2a3860a70c8b5a2d7
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-11 14:41:01 -0500

    20251111-upload route and controller

---
+++ app/controllers/results_csvs_controller.rb
@@ -0,0 +1,4 @@
+class ResultsCsvsController < ApplicationController
+  before_action :set_roll, only: %i[ new create ]
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
         get "edit_name"
       end
       resources :results, shallow: true
+      resources :results_csvs
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
