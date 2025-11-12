7e5bfc82b23b607ecebbf3f6a5cb07568a6d9c68
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-12 16:36:48 -0500

    main rubocop'd

--- test/services/results_img_processor_test.rb
+++ test/services/results_img_processor_test.rb
@@ -5,12 +5,11 @@ require 'test_helper'
 class ResultsImgProcessorTest < ActiveSupport::TestCase
   setup do
     @roll = rolls(:encounter_forest)
-    @image_file = Rails.root.join( 'test/ocr/forest_encounters_p154.png')
+    @image_file = Rails.root.join('test/ocr/forest_encounters_p154.png')
   end
   test 'creates results from OCR image' do
     assert_difference('@roll.results.count', 23) do
-
-    ResultsImgProcessor.new(@roll, @image_file).call
+      ResultsImgProcessor.new(@roll, @image_file).call
     end
   end
 end
