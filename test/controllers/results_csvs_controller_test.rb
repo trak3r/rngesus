45512fdc9f33002d1a19b57a02bdd9ed242f5f8d
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-12 14:17:44 -0500

    main test for csv uploader

---
+++ test/controllers/results_csvs_controller_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class ResultsCsvsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @roll = rolls(:encounter_forest)
+    @file_path = Rails.root.join("test/csv/forest_encounters_p154.csv")
+  end
+
+  test "creates results from uploaded CSV" do
+    file = fixture_file_upload(@file_path, "text/csv")
+
+    assert_difference("@roll.results.count", 23) do
+      post roll_results_csvs_path(@roll), params: {
+        results_csv: { file: file }
+      }
+    end
+
+    assert_redirected_to roll_path(@roll)
+    follow_redirect!
+    assert_match "CSV processed successfully", response.body
+  end
+end
