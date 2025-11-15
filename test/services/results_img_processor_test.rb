1e19298a7c748bdf1199a38fc711fdc6a3d55810
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-15 09:48:47 -0500

    main setting up test for more complicated screenshot

--- test/services/results_img_processor_test.rb
+++ test/services/results_img_processor_test.rb
@@ -14,10 +14,21 @@ class ResultsImgProcessorTest < ActiveSupport::TestCase
     end
   end
 
-  test 'handle a more complicated image with 3 columns and multi-line rows' do
+  test 'handles a more complicated image with 3 columns and multi-line rows' do
     image_file = Rails.root.join('test/ocr/carousing_outcome_p93.png')
+
     assert_difference('@roll.results.count', 14) do
       ResultsImgProcessor.new(@roll, image_file).call
     end
+
+    @roll.reload
+
+    first_result = @roll.results.first
+    last_result  = @roll.results.last
+
+    assert first_result.name.ends_with?('bed'),
+           "Expected first result to end with 'bed'"
+    assert last_result.name.ends_with?('approach'),
+           "Expected last result to end with 'approach'"
   end
 end
