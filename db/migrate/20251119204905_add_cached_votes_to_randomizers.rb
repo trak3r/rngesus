26268a74806816f71e9e3af966a2223566f73398
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-19 16:04:43 -0500

    20251119-categorical it's a sin to change an already-run migration but AI don't care

--- db/migrate/20251119204905_add_cached_votes_to_randomizers.rb
+++ db/migrate/20251119204905_add_cached_votes_to_randomizers.rb
@@ -4,5 +4,12 @@ class AddCachedVotesToRandomizers < ActiveRecord::Migration[8.1]
     add_column :randomizers, :cached_votes_score, :integer, default: 0
     add_column :randomizers, :cached_votes_up, :integer, default: 0
     add_column :randomizers, :cached_votes_down, :integer, default: 0
+    
+    # Update cached votes for existing randomizers
+    reversible do |dir|
+      dir.up do
+        Randomizer.find_each(&:update_cached_votes)
+      end
+    end
   end
 end
