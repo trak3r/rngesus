da740dce8e08dc8fe9b01542aa95fc3fbdd35628
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-24 14:57:23 -0500

    20251124-style rubocop'd

--- test/system/randomizers_index_test.rb
+++ test/system/randomizers_index_test.rb
@@ -1,4 +1,6 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class RandomizersIndexTest < ApplicationSystemTestCase
   setup do
@@ -6,35 +8,35 @@ class RandomizersIndexTest < ApplicationSystemTestCase
     @randomizer = randomizers(:encounter)
   end
 
-  test "randomizer name is truncated" do
+  test 'randomizer name is truncated' do
     # Create a randomizer with a long name
-    long_name = "This is a very long randomizer name that should definitely be truncated because it is way too long to fit on a single line or even two lines"
+    long_name = 'This is a very long randomizer name that should definitely be truncated because it is way too long to fit on a single line or even two lines'
     Randomizer.create!(name: long_name, user: @user)
 
     visit randomizers_path
-    
+
     # Check for the presence of the line-clamp class
-    assert_selector "h2.line-clamp-1", text: long_name
+    assert_selector 'h2.line-clamp-1', text: long_name
   end
 
-  test "rolls are limited to 2 and show more indicator" do
+  test 'rolls are limited to 2 and show more indicator' do
     # Create a randomizer with many rolls
-    randomizer = Randomizer.create!(name: "Many Rolls", user: @user)
-    5.times { |i| randomizer.rolls.create!(name: "Roll #{i}", dice: "1d6") }
+    randomizer = Randomizer.create!(name: 'Many Rolls', user: @user)
+    5.times { |i| randomizer.rolls.create!(name: "Roll #{i}", dice: '1d6') }
 
     visit randomizers_path
 
     within "#randomizer_#{randomizer.id}" do
-      assert_text "Roll 0"
-      assert_text "Roll 1"
-      assert_no_text "Roll 2"
-      assert_text "...and more"
+      assert_text 'Roll 0'
+      assert_text 'Roll 1'
+      assert_no_text 'Roll 2'
+      assert_text '...and more'
     end
   end
 
-  test "cards have consistent layout structure" do
+  test 'cards have consistent layout structure' do
     visit randomizers_path
     # Verify the grid structure is present (checking for the class we plan to add)
-    assert_selector ".grid" 
+    assert_selector '.grid'
   end
 end
