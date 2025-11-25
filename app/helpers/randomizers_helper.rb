96322b4146739c6c1396889d3e17e26ff4827345
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-11-25 11:44:29 -0500

    main another card image

--- app/helpers/randomizers_helper.rb
+++ app/helpers/randomizers_helper.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 module RandomizersHelper
-  DND_ARTWORKS = %w[dragon wizard battle moon_bats].freeze
+  DND_ARTWORKS = %w[
+  dragon 
+  wizard 
+  battle 
+  moon_bats 
+  crude_map
+  ].freeze
 
   def random_card_artwork
     DND_ARTWORKS.sample
   end
 
   def random_artwork_position
-    rand(1..5)
+    rand(2..6)
   end
 end
