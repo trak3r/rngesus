44267cc54c2babafcc5f37e5b91c128435f75ba4
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-12-08 15:39:48 -0500

    20251208-good we'll need to download a copy of the database to test this migration before merging and deploying

---
+++ db/migrate/20251208170000_migrate_randomizers_data.rb
@@ -0,0 +1,89 @@
+class MigrateRandomizersData < ActiveRecord::Migration[8.1]
+  class Randomizer < ActiveRecord::Base; end
+  class Roll < ActiveRecord::Base
+    include Discard::Model
+  end
+  class RollTag < ActiveRecord::Base; end
+  class Vote < ActiveRecord::Base; end
+  class Tag < ActiveRecord::Base
+    has_many :randomizer_tags
+    has_many :randomizers, through: :randomizer_tags
+  end
+  class RandomizerTag < ActiveRecord::Base
+    belongs_to :randomizer
+    belongs_to :tag
+  end
+
+  # Re-open Randomizer to add associations for the migration
+  class Randomizer < ActiveRecord::Base
+    has_many :randomizer_tags
+    has_many :tags, through: :randomizer_tags
+    belongs_to :user
+  end
+
+  class User < ActiveRecord::Base; end
+
+  def up
+    # Safety check for environments where randomizers table is already gone
+    return unless table_exists?(:randomizers)
+
+    puts "Starting migration..."
+    
+    Randomizer.find_each do |randomizer|
+      puts "Processing Randomizer: #{randomizer.name} (ID: #{randomizer.id})"
+      
+      # Helper to get all rolls but prioritize kept ones for slug inheritance
+      # Using local Roll class
+      kept_rolls = Roll.where(randomizer_id: randomizer.id).kept.order(:created_at)
+      discarded_rolls = Roll.where(randomizer_id: randomizer.id).discarded.order(:created_at)
+      
+      rolls = kept_rolls.to_a + discarded_rolls.to_a
+      
+      rolls.each_with_index do |roll, index|
+        
+        # Prepare attributes for update_columns
+        attrs = { user_id: randomizer.user_id }
+        
+        if index == 0
+          # Primary Roll: Inherit slug and votes
+          attrs[:slug] = randomizer.slug
+          attrs[:cached_votes_total] = randomizer.cached_votes_total
+          attrs[:cached_votes_score] = randomizer.cached_votes_score
+          attrs[:cached_votes_up] = randomizer.cached_votes_up
+          attrs[:cached_votes_down] = randomizer.cached_votes_down
+          
+          # Move Votes
+          Vote.where(votable_type: 'Randomizer', votable_id: randomizer.id)
+              .update_all(votable_type: 'Roll', votable_id: roll.id)
+          
+          puts "  - Primary Roll (ID: #{roll.id}): Inheriting slug '#{randomizer.slug}'."
+        else
+          # Generate new slug
+          new_slug = nil
+          loop do
+            new_slug = SecureRandom.alphanumeric(5)
+            # Check against local Roll class
+            break unless Roll.exists?(slug: new_slug) && new_slug != randomizer.slug 
+          end
+          attrs[:slug] = new_slug
+          puts "  - Secondary Roll (ID: #{roll.id}): Generated new slug '#{new_slug}'."
+        end
+        
+        # Bypass validations and readonly checks
+        roll.update_columns(attrs)
+        
+        # Copy tags
+        randomizer.tags.each do |tag|
+          RollTag.find_or_create_by!(roll_id: roll.id, tag_id: tag.id)
+        end
+        
+        puts "  - Processed Roll #{roll.id}"
+      end
+    end
+    puts "Migration complete!"
+  end
+
+  def down
+    # Data migration usually doesn't need a down method as it moves data forward
+  end
+end
