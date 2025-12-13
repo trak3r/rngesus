649b8848db857a302b0bc939376e1ef059894591
Thomas "Teflon Ted" Davis <github@rudiment.net>
2025-12-13 12:40:47 -0500

    20251213-better migration draft

---
+++ db/migrate/20251213173700_better_tag_variety.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class BetterTagVariety < ActiveRecord::Migration[8.1]
+  def change
+    %i[
+      consumable
+      downtime
+      effect
+      encounter
+      event
+      location
+      loot
+      npc
+      plot
+      urban
+      weather
+    ].each do |tag_name|
+      Tag.create!(name: tag_name.titleize)
+    end
+    Tag.find(1).update(name: 'wilderness')
+  end
+end
